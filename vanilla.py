de48eb7e4596b071cbdc5abe00507c68b27692b6
mykysyk <mykysyk@gmail.com>
2020-02-20 11:25:14 +0900

    Update vanilla.py

--- vanilla.py
+++ vanilla.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 class FastFoodSystem:
+    """ ファーストフードレジシステム """
     def set_menu(self, menu):
         """ メニューを登録 """
         pass
@@ -22,18 +23,22 @@ class FastFoodSystem:
     def get_receipt(self):
         """ 領収書を発行 """
         pass
-      
+
 menu_dict = {'コーラー': 100, 'ハンバーガー': 110, 'ポテト': 100}
 macdonald = FastFoodSystem()
 macdonald.set_menu(menu_dict)
 macdonald.set_order('ハンバーガー')
 macdonald.set_order('ポテト')
 macdonald.set_order('コーラー')
-macdonald.get_order()        # ['ハンバーガー', 'ポテト', 'コーラー']
-macdonald.get_total_price()  # 310
+order = macdonald.get_order()
+print(order)                        # ['ハンバーガー', 'ポテト', 'コーラー']
+total_price = macdonald.get_total_price()
+print(total_price)                 # 310
 macdonald.set_deposit(500)
-macdonald.get_change()       # 190
-macdonald.get_receipt()      # {'change': 190,
-                             #  'deposit': 500,
-                             #  'order_list': ['ハンバーガー', 'ポテト', 'コーラー'],
-                             #  'total_price': 310}
+change = macdonald.get_change()
+print(change)                       # 190
+receipt = macdonald.get_receipt()
+print(receipt)                      # {'change': 190,
+                                    #  'deposit': 500,
+                                    #  'order_list': ['ハンバーガー', 'ポテト', 'コーラー'],
+                                    #  'total_price': 310}
